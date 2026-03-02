ef380b32e670f20bb9db2b81b6e2778a042ed8a1
Lemur <gunicoletti2007@proton.me>
2026-03-02 16:38:30 -0300

    Seeing if gamja works

---
+++ Lemur's website/gamja/eslint.config.js
@@ -0,0 +1,33 @@
+import globals from "globals";
+import js from "@eslint/js";
+import stylisticJs from "@stylistic/eslint-plugin-js";
+
+export default [
+	{
+		ignores: ["dist/"],
+	},
+	js.configs.recommended,
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				"process": "readonly",
+			},
+		},
+		plugins: { "@stylistic/js": stylisticJs },
+		rules: {
+			"no-case-declarations": "off",
+			"no-unused-vars": ["error", {
+				args: "none",
+				caughtErrorsIgnorePattern: "^_",
+				destructuredArrayIgnorePattern: "^_",
+			}],
+			"no-var": "error",
+			"@stylistic/js/indent": ["warn", "tab"],
+			"@stylistic/js/quotes": ["warn", "double"],
+			"@stylistic/js/semi": "warn",
+			"@stylistic/js/comma-dangle": ["warn", "always-multiline"],
+			"@stylistic/js/arrow-parens": "warn",
+		},
+	},
+];
